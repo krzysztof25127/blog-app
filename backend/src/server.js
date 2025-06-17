e3cf216c68b99fd4cd307025bcd979378bd3d618
krzysztof25127 <25127@edu.amw.gdynia.pl>
2025-06-17 06:07:29 +0200

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,34 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+
 import postsRoutes from "./routes/postsRoutes.js";
 import { connectDB } from "./config/db.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(
-  cors({
-    origin: ["http://localhost:5173"],
-  })
-);
+const __dirname = path.resolve();
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: ["http://localhost:5173"],
+    })
+  );
+}
+
 app.use(express.json());
 
 app.use("/api/posts", postsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB()
   .then(() => {
